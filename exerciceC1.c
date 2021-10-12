d3f91a5e3c49a5388cb73eb65c946e7ec9f77556
THom1331 <90909474+THom1331@users.noreply.github.com>
2021-10-12 09:39:30 +0200

    Create exerciceC1.c

---
+++ exerciceC1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+
+ {   int a, b; //déclaration de varaible
+
+    printf("saisissez un premier nombre\n"); //affichage
+
+        scanf("%i", &a); // attribution d'une valeur entrée par l'utilisateur pour a
+
+    printf("saisissez un deuxième nombre\n"); //affichage
+
+        scanf("%i", &b); // attribution d'une valeur entrée par l'utilisateur pour b
+
+    printf("le produit de %i et %i est :%i\n", a, b, a * b); //opération + résultat
+ }
